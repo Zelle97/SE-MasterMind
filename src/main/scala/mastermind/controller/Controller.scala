59eac05978c4d20756d697dfecfd0e07627310e2
Fabian Zeller <zeller.fabian.97@web.de>
2020-12-09 14:51:45 +0100

    Added Command Pattern With UndoManager

--- src/main/scala/mastermind/controller/Controller.scala
+++ src/main/scala/mastermind/controller/Controller.scala
@@ -1,28 +1,39 @@
 package mastermind.controller
 
 import mastermind.model.{Attempt, Color, ColorFactory, GameData}
-import mastermind.util.{GameOver, InGame, Observable, Win}
+import mastermind.util.{GameOver, InGame, Observable, UndoManager, Win}
 
 class Controller(var gameData: GameData, var turn: Int = 0) extends Observable {
+
+  private val undoManager = new UndoManager
 
   def addAttempt(input: String): Unit = {
     //GameState.handle(InGame())
     val colors = input.split(" ").toVector
     val attempt = Attempt(colors.map(color => ColorFactory.getColor(color)))
-    gameData = gameData.updateAttempt(gameData.attempts.size - turn - 1, attempt)
-    turn = turn + 1
-
+    undoManager.doStep(new AddCommand(gameData, attempt, this))
     notifyObservers
 
-    if (gameData.attempts(gameData.attempts.size - turn).getCorrectPositions(gameData.solution) == 4){
+    if (gameData.attempts(gameData.attempts.size - turn).getCorrectPositions(gameData.solution) == 4) {
       println(GameState.handle(Win()))
       System.exit(1)
     }
 
-    if (turn == gameData.attempts.size){
+    if (turn == gameData.attempts.size) {
       println(GameState.handle(GameOver()))
       System.exit(1)
     }
+
+  }
+
+  def undo: Unit = {
+    undoManager.undoStep
+    notifyObservers
+  }
+
+  def redo: Unit = {
+    undoManager.redoStep
+    notifyObservers
   }
 
   def gameToString(): String = {
