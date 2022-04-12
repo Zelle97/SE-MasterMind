1b838d7d5f6490e20a00d99094c30b635e62fee9
gerhard <gerhard_goetz@gmx.net>
2022-04-12 22:07:01 +0200

    recursion board

--- src/main/scala/mastermind/model/GameBoard.scala
+++ src/main/scala/mastermind/model/GameBoard.scala
@@ -2,11 +2,11 @@ package mastermind.model
 
 import mastermind.model.gameDataComponent.GameDataInterface
 
-case class GameBoard(gamedata: GameDataInterface, gamefield: String = "") {
+import scala.annotation.tailrec
 
+case class GameBoard(gamedata: GameDataInterface, gamefield: String = "") {
   def gameToString(): GameBoard =
     GameBoard(gamedata, new StringBuilder(concatTitle()).append(concatLineBreak()).append(concatEachSlotField()).toString())
-
   def concatEachSlotField(): String =
     val builder = new StringBuilder
     gamedata.attempts.indices.foreach(i => {
@@ -24,39 +24,35 @@ case class GameBoard(gamedata: GameDataInterface, gamefield: String = "") {
       builder.append(concatLineBreak())
     })
     builder.toString()
-
-
   def forEachSlot(slotField: Int): String =
     val builder = new StringBuilder
-    for (i <- 0 to 3) {
-      builder.append(concatVerticalLine())
-      builder.append(gamedata.getAttempt(slotField).userPickedColors(i).getColor)
-      builder.append(concatVerticalLine())
-    }
+    loop(0)
+    @tailrec
+    def loop(i: Int): Unit =
+      if i <= 3 then
+        builder.append(concatVerticalLine())
+        builder.append(gamedata.getAttempt(slotField).userPickedColors(i).getColor)
+        builder.append(concatVerticalLine())
+        loop(i + 1)
     builder.toString()
-
-
   def concatSlotPadding(): String =
     val builder = new StringBuilder
-    for (i <- 0 to 3) {
-      builder.append(concatVerticalLine())
-      builder.append(concatEmptySpace())
-      builder.append(concatVerticalLine())
-    }
+    loop(0)
+    @tailrec
+    def loop(i: Int): Unit =
+      if i <= 3 then
+        builder.append(concatVerticalLine())
+        builder.append(concatEmptySpace())
+        builder.append(concatVerticalLine())
+        loop(i + 1)
     builder.toString()
-
-
   def concatTitle(): String = "\t\t\t\tMaster Mind"
   def concatVerticalLine(): String = "|"
   def concatHorizontalLine(): String = "------------------------------------------------"
   def concatEmptySpace(): String = "          "
-
   def concatCorrectPositions(slotField: Int): String =
     " Correct Positions: " + gamedata.getAttempt(slotField).getCorrectPositions(gamedata.solution)
-
   def concatCorrectColors(slotField: Int): String =
     " Correct Colors: " + gamedata.getAttempt(slotField).getCorrectColors(gamedata.solution)
-
-  def concatLineBreak(): String =
-    "\n"
+  def concatLineBreak(): String = "\n"
 }
