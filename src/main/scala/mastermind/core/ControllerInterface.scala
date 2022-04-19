afed1ca73f7d47e84b059d04bad9539a933a39e1
Fabian Zeller <zeller.fabian.97@web.de>
2022-04-19 15:30:10 +0200

    Rearranged files

--- src/main/scala/mastermind/core/ControllerInterface.scala
+++ src/main/scala/mastermind/core/ControllerInterface.scala
@@ -8,10 +8,16 @@ import scala.util.Try
 trait ControllerInterface() extends Publisher {
   val gameState: GameState
   val colorFactory: ColorFactoryInterface
+
   def setDifficulty(difficultyInput: String): Try[String]
+
   def addAttempt(input: String): Try[String]
+
   def undo(): Unit
+
   def redo(): Unit
+
   def save(): Unit
+
   def load(): Unit
 }
