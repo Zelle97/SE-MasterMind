e0fe173f99a2fa4ecd2743d5babdee568d41fed8
Fabian Zeller <zeller.fabian.97@web.de>
2022-04-25 14:52:18 +0200

    Implemented Akka and did some routing for core

--- build.sbt
+++ build.sbt
@@ -11,6 +11,14 @@ lazy val root = (project in file(".")).aggregate(view, core, persistence)
 
 mainClass := Some("mastermind.MasterMind")
 
+val AkkaVersion = "2.6.8"
+val AkkaHttpVersion = "10.2.9"
+libraryDependencies ++= Seq(
+  ("com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion).cross(CrossVersion.for3Use2_13),
+  ("com.typesafe.akka" %% "akka-stream" % AkkaVersion).cross(CrossVersion.for3Use2_13),
+  ("com.typesafe.akka" %% "akka-http" % AkkaHttpVersion).cross(CrossVersion.for3Use2_13),
+  ("com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion).cross(CrossVersion.for3Use2_13)
+)
 libraryDependencies += "org.scalactic" %% "scalactic" % "3.2.11"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.11" % "test"
 libraryDependencies += "org.scala-lang.modules" %% "scala-swing" % "3.0.0"
---
+++ src/main/scala/mastermind/core/CoreInterface.scala
@@ -0,0 +1,71 @@
+package mastermind.core
+
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.*
+import akka.http.scaladsl.server.Directives.*
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.unmarshalling.Unmarshaller
+import akka.http.scaladsl.model.HttpEntity
+import com.google.inject.Guice
+import mastermind.MasterMindModule
+import play.api.libs.json.Json
+import spray.json.*
+import spray.json.DefaultJsonProtocol.*
+
+import scala.io.StdIn
+
+object CoreInterface {
+
+  def main(args: Array[String]): Unit = {
+
+    implicit val system = ActorSystem(Behaviors.empty, "my-system")
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.executionContext
+
+
+    val injector = Guice.createInjector(new MasterMindModule)
+    val controller = injector.getInstance(classOf[ControllerInterface])
+
+    def gameRoutes:Route = {
+      path("game") {
+        get {
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, controller.gameState.gameData.toString()))
+        }
+      }
+    }
+
+    def attemptRoutes:Route = {
+      path("attempt") {
+        post {
+          controller.addAttempt("red red red red")
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "Success"))
+        }
+      }
+    }
+
+    def difficultyRoutes:Route = {
+      path("difficulty") {
+        post {
+          entity(as[String]) { str =>
+            // TODO Difficulty as Json -> Serialize new class for difficulty
+            println(str.parseJson.prettyPrint)
+            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "Success"))
+          }
+        }
+      }
+    }
+
+    val routes = gameRoutes ~ attemptRoutes ~ difficultyRoutes
+
+    val bindingFuture = Http().newServerAt("localhost", 8080).bind(routes)
+
+    println(s"Server now online. Please navigate to http://localhost:8080/hello\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
+  }
+}
